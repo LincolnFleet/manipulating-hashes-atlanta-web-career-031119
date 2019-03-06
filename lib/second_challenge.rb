9af5f85e82fe2db4e80e2ba241c79d3cfa731576
Travis Martin <travisemerymartin@gmail.com>
2019-03-06 18:18:34 +0000

    Automatically backed up by Learn

---
+++ lib/second_challenge.rb
@@ -0,0 +1,11 @@
+def second_challenge
+  groceries = {
+   dairy: ["milk", "yogurt", "cheese"],
+   vegetable: ["carrots", "broccoli", "cucumbers"],
+   meat: ["chicken", "steak", "salmon"],
+   grains: ["rice", "pasta"]
+  }
+
+  #code your solution here!
+  nom_noms=groceries.values.flatten
+end
